559c24ccde5e4a4b0498d0fc4e0e44575cd0499c
mohamedahamedabdelazzem-crypto <mohamedahamed.abdelazzem@gmail.com>
2026-04-03 17:34:45 +0200

    generate notes_model.g

--- lib/models/notes_model.dart
+++ lib/models/notes_model.dart
@@ -1,6 +1,12 @@
-class NotesModel {
+import 'package:hive_flutter/hive_flutter.dart';
+part 'notes_model.g.dart';
+@HiveType(typeId: 0)
+class NotesModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
 
   NotesModel({ required this.title, required this.subTitle, required this.date});
